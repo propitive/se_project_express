b89fff15362a6218810ec5b1e2e6086e30cdc83e
Propitive <propitive@gmail.com>
2023-06-06 09:27:44 -0500

    project 13 - corrected auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,48 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
-const { handleError, ERROR_CODES } = require("../utils/errors");
+const { handleError } = require("../utils/errors");
+
+// module.exports = (req, res, next) => {
+//   try {
+//     const { authorization } = req.headers;
+
+//     if (!authorization || !authorization.startsWith("Bearer ")) {
+//       return res
+//         .status(ERROR_CODES.Unauthorized)
+//         .send({ message: "Authorization required" });
+//     }
+
+//     const token = authorization.replace("Bearer ", "");
+//     let payload;
+
+//     try {
+//       payload = jwt.verify(token, JWT_SECRET);
+//     } catch (err) {
+//       return res
+//         .status(ERROR_CODES.Unauthorized)
+//         .send({ message: "Authorization required" });
+//     }
+//     req.user = payload;
+//     next();
+//   } catch (err) {
+//     handleError(err, res);
+//   }
+// };
 
 module.exports = (req, res, next) => {
   try {
-    const { authorization } = req.headers;
+    const authorization = req.get("authorization");
 
-    if (!authorization || !authorization.startsWith("Bearer ")) {
-      return res
-        .status(ERROR_CODES.Unauthorized)
-        .send({ message: "Authorization required" });
+    if (!authorization) {
+      const error = new Error("Authorization token is missing.");
+      error.name = "UnauthorizedError";
+      throw error;
     }
 
     const token = authorization.replace("Bearer ", "");
-    let payload;
 
-    try {
-      payload = jwt.verify(token, JWT_SECRET);
-    } catch (err) {
-      return res
-        .status(ERROR_CODES.Unauthorized)
-        .send({ message: "Authorization required" });
-    }
+    const payload = jwt.verify(token, JWT_SECRET);
+
     req.user = payload;
     next();
   } catch (err) {
