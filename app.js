a671f37d645537c3ab0d86d4677fb5960b6801ac
Propitive <propitive@gmail.com>
2023-07-05 17:22:23 -0500

    updated homepage field in package.json

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.use(cors());
 
 app.use(requestLogger);
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use(routes);
 app.use(errorLogger);
 app.use(errors());
